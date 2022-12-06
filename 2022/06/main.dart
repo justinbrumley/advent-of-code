3df111f91f3cfe2587543d63f3345a0fc822db47
Justin Brumley <justin@megahook.in>
2022-12-06 09:36:33 -0600

    chore(06): provide sequence length to func + use toSet on runes

--- 2022/06/main.dart
+++ 2022/06/main.dart
@@ -4,44 +4,16 @@ String getInput() {
   return new File('./input').readAsStringSync();
 }
 
-int getStartOfPacket(String input) {
-  for (int i = 3; i < input.length; i++) {
-    // Grab previous 4 characters
-    final code = input.substring(i - 3, i + 1);
+int getStartOfSequence(String input, int seqLen) {
+  for (int i = seqLen - 1; i < input.length; i++) {
+    // Grab previous X characters
+    final code = input.substring(i - seqLen + 1, i + 1);
 
     // Check for any duplicates
-    final runes = code.runes.toList();
+    final runes = code.runes.toSet();
 
-    var uniqueRunes = Set<int>();
-    for (var i = 0; i < runes.length; i++) {
-      uniqueRunes.add(runes[i]);
-    }
-
-    if (uniqueRunes.length == 4) {
-      // Find 4 unique runes, so return index + 1 to signify the count
-      return i + 1;
-    }
-  }
-
-  return -1;
-}
-
-// Same as start of packet, but requires 14 unique characters instead of 4
-int getStartOfMessage(String input) {
-  for (int i = 13; i < input.length; i++) {
-    // Grab previous 14 characters
-    final code = input.substring(i - 13, i + 1);
-
-    // Check for any duplicates
-    final runes = code.runes.toList();
-
-    var uniqueRunes = Set<int>();
-    for (var i = 0; i < runes.length; i++) {
-      uniqueRunes.add(runes[i]);
-    }
-
-    if (uniqueRunes.length == 14) {
-      // Find 14 unique runes, so return index + 1 to signify the count
+    if (runes.length == seqLen) {
+      // Find X unique runes, so return index + 1 to signify the count
       return i + 1;
     }
   }
@@ -51,8 +23,8 @@ int getStartOfMessage(String input) {
 
 void main() {
   final input = getInput();
-  final startOfPacket = getStartOfPacket(input);
-  final startOfMessage = getStartOfMessage(input);
+  final startOfPacket = getStartOfSequence(input, 4);
+  final startOfMessage = getStartOfSequence(input, 14);
 
   print('Start of Packet: ${startOfPacket}');
   print('Start of Message: ${startOfMessage}');
