f845896429ebc1500f1b6137bbf4e5083083d9c8
Justin Brumley <justin@megahook.in>
2024-12-03 09:20:59 -0600

    feat(2024:03): add solution (regexp FTW)

---
+++ 2024/cmd/day3.go
@@ -0,0 +1,52 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"regexp"
+	"strconv"
+
+	"github.com/justinbrumley/advent-of-code/2024/utils"
+	"github.com/spf13/cobra"
+)
+
+// day3Cmd represents the day3 command
+var day3Cmd = &cobra.Command{
+	Use:   "day3",
+	Short: "Advent of Code 2024 - Day 3",
+	Run: func(cmd *cobra.Command, args []string) {
+		lines, err := utils.GetInput("inputs/day3")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		re := regexp.MustCompile(`(mul|don't|do)\(((\d{1,3}),(\d{1,3}))?\)`)
+
+		enabled := true
+		total := 0
+
+		for _, line := range lines {
+			instructions := re.FindAllStringSubmatch(line, -1)
+
+			for _, instruction := range instructions {
+				cmd := instruction[1]
+
+				if cmd == "mul" && enabled {
+					x, _ := strconv.Atoi(instruction[3])
+					y, _ := strconv.Atoi(instruction[4])
+					total += (x * y)
+				} else if cmd == "don't" {
+					enabled = false
+				} else if cmd == "do" {
+					enabled = true
+				}
+			}
+		}
+
+		fmt.Printf("Sum of enabled mul() instructions: %v\n", total)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(day3Cmd)
+}
